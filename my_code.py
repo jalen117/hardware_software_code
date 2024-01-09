ad8f3a268dadb709eaeec1bbd192387a0514eb04
jalen117 <jalen4117@gmail.com>
2024-01-09 10:24:10 -0500

    first commit

---
+++ my_code.py
@@ -0,0 +1,11 @@
+def main():
+    name = "Jalen Lanier"
+    print ("We want to know if you like programming!")
+    print()
+    print("Do you like programming" + name + "?")
+    answer = input()
+    print ("Great! You said" + answer + "?")
+    print ("Let's start learning some Python today")
+
+if __name__ == "__main__":
+    main()
---
+++ your_jalen.py
@@ -0,0 +1,10 @@
+def main():
+name = input ("What is your name?")
+print ("We want to know if you like programming!")
+answer = input ("Do you like programming" + name + "?")
+print ('Great! You said"' + answer + '"?')
+print ("Let's Learn some Python today")
+
+
+if __name__ == "__main__":
+     main()
