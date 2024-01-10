56d00c36d3cc97a5de748aac79bfc55dde82993b
jalen117 <jalen4117@gmail.com>
2024-01-10 11:20:41 -0500

    Today's work

--- your_jalen.py
+++ your_jalen.py
@@ -3,8 +3,7 @@ name = input ("What is your name?")
 print ("We want to know if you like programming!")
 answer = input ("Do you like programming" + name + "?")
 print ('Great! You said"' + answer + '"?')
-print ("Let's Learn some Python today")
-
+print ("Let's learn some Python today")
 
 if __name__ == "__main__":
      main()
